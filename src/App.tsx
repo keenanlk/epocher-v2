6d66e8618a1042b5bb765d7ee0622fc28e66f9cd
Keenan Kaufman <keenanlkaufman@gmail.com>
2024-02-02 16:11:26 -0600

    chore: add release action

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
-
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
     <div className="w-screen h-screen bg-white">
       <h1 className="text-black">Hello World</h1>
