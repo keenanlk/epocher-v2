99751e2ef3afc4c30a9f968cbd49ec8f6da56787
Keenan Kaufman <keenanlkaufman@gmail.com>
2024-02-02 22:01:41 -0600

    chore: initial logic and frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { formatInTimeZone } from "date-fns-tz";
+import { Calendar } from "primereact/calendar";
+
 function App() {
+  const [date, setDate] = useState<Date>(new Date());
+
+  const ref = React.useRef(null);
+
+  function getUnixTimeFromDate() {
+    return Math.floor(date.getTime() / 1000);
+  }
+
+  function onUnixTimeChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    const unixTime = parseInt(value);
+    if (isNaN(unixTime)) {
+      return;
+    }
+    setDate(new Date(unixTime * 1000));
+  }
+
+  useEffect(() => {
+    debugger;
+    if (date.getMonth() && ref.current) {
+      (ref.current as Calendar).updateViewDate(null, date);
+    }
+  }, [date, ref]);
+
+  const [timeZone] = useState<string>(
+    Intl.DateTimeFormat().resolvedOptions().timeZone,
+  );
+
+  function getDateStringInLocalTimeZone() {
+    return formatInTimeZone(date, timeZone, "yyyy-MM-dd hh:mm:ss a zzz");
+  }
+
+  function getDateStringInUTC() {
+    return formatInTimeZone(date, "UTC", "yyyy-MM-dd hh:mm:ss a zzz");
+  }
+
+  function getDateISOString() {
+    return date.toISOString();
+  }
+
   return (
-    <div className="w-screen h-screen bg-white">
-      <h1 className="text-black">Hello World</h1>
+    <div className="w-screen h-screen bg-black bg-opacity-40">
+      <div className="grid gap-4 grid-cols-1 p-8">
+        <div>
+          <input
+            type="number"
+            value={getUnixTimeFromDate()}
+            onChange={onUnixTimeChange}
+            className=" unix-input"
+          />
+        </div>
+        <div>
+          <p className="text-white">{getDateStringInLocalTimeZone()}</p>
+        </div>
+        <div>
+          <p className="text-white">{getDateStringInUTC()}</p>
+        </div>
+        <div>
+          <p className="text-white">{getDateISOString()}</p>
+        </div>
+        <div>
+          <Calendar
+            ref={ref}
+            value={date}
+            onChange={(e) => e.value && setDate(e.value)}
+            inline
+            showTime={true}
+          />{" "}
+        </div>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/bootstrap4-dark-blue/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>,
 );
