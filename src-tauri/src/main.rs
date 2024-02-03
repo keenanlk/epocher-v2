c506d620d71dcc6b46929fe47207f396035dc8fb
Keenan Kaufman <keenanlkaufman@gmail.com>
2024-02-02 22:27:46 -0600

    chore: dont show dock icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
     let system_tray_menu = SystemTrayMenu::new().add_item(quit);
     tauri::Builder::default()
+        .setup(|app| Ok(app.set_activation_policy(tauri::ActivationPolicy::Accessory)))
         .plugin(tauri_plugin_positioner::init())
         .system_tray(SystemTray::new().with_menu(system_tray_menu))
         .on_system_tray_event(|app, event| {
