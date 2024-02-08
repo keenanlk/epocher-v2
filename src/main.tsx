95c409427461e6c3ba7cb9b7f407ee6ff8c24bb6
Keenan Kaufman <keenanlkaufman@gmail.com>
2024-02-07 19:35:13 -0600

    chore: remove some dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import "primereact/resources/themes/bootstrap4-dark-blue/theme.css";
 import { Flowbite } from "flowbite-react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
